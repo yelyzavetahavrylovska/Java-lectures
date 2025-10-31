a0292347ecde8e3d38f9355db04f2cc811b7246e
yelyzavetahavrylovska <131909323+yelyzavetahavrylovska@users.noreply.github.com>
2025-10-31 22:34:21 +0100

    Delete lesson2 directory

--- lesson2/Example11.java
+++
@@ -1,9 +0,0 @@
-package Module1.lesson2;
-
-public class Example11 {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = a + 10;
-        System.out.println(b);
-    }
-}
